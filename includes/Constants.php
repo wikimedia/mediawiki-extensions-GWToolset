d3761275558a0fe397364a3616847dc65d324f23
dan-nl <d_entous@yahoo.com>
2014-01-23 19:18:04 -0800

    updating version nr from 0.1.0 to 0.1.1
    Change-Id: I452382eb9c9c425ea6b9387b7d67df8e2275dc13

--- includes/Constants.php
+++ includes/Constants.php
@@ -12,7 +12,7 @@ namespace GWToolset;
 class Constants {
 
 	const EXTENSION_NAME = 'GWToolset';
-	const EXTENSION_VERSION = '0.1.0';
+	const EXTENSION_VERSION = '0.1.1';
 	const REQUIRED_MEDIAWIKI_VERSION = '1.22c';
 	const USE_FILEBACKEND = true;
 
