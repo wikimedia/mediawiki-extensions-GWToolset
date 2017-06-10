c820eaf203010cdd70fb1348269312a2c1c51118
Umherirrender <umherirrender_de.wp@web.de>
2017-06-10 12:22:14 +0200

    Use short array syntax in alias file
    Change-Id: I6fcbe52e3c952afac3a4a997d0730387e3a47658

--- GWToolset.alias.php
+++ GWToolset.alias.php
@@ -6,66 +6,65 @@
  * @ingroup Extensions
  * @license GNU General Public License 3.0 http://www.gnu.org/licenses/gpl.html
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'GWToolset' => array( 'GWToolset' ),
-);
+$specialPageAliases['en'] = [
+	'GWToolset' => [ 'GWToolset' ],
+];
 
 /** Arabic (العربية) */
-$specialPageAliases['ar'] = array(
-	'GWToolset' => array( 'أدوات_ج_و' ),
-);
+$specialPageAliases['ar'] = [
+	'GWToolset' => [ 'أدوات_ج_و' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$specialPageAliases['arz'] = array(
-	'GWToolset' => array( 'ادوات_ج_و' ),
-);
+$specialPageAliases['arz'] = [
+	'GWToolset' => [ 'ادوات_ج_و' ],
+];
 
 /** Western Balochi (بلوچی رخشانی) */
-$specialPageAliases['bgn'] = array(
-	'GWToolset' => array( 'جی_ڈبلیو_ٹی_ئی_وسایلان' ),
-);
+$specialPageAliases['bgn'] = [
+	'GWToolset' => [ 'جی_ڈبلیو_ٹی_ئی_وسایلان' ],
+];
 
 /** Zazaki (Zazaki) */
-$specialPageAliases['diq'] = array(
-	'GWToolset' => array( 'GWSazhalet' ),
-);
+$specialPageAliases['diq'] = [
+	'GWToolset' => [ 'GWSazhalet' ],
+];
 
 /** Persian (فارسی) */
-$specialPageAliases['fa'] = array(
-	'GWToolset' => array( 'ابزارهای_جی‌دبلیو' ),
-);
+$specialPageAliases['fa'] = [
+	'GWToolset' => [ 'ابزارهای_جی‌دبلیو' ],
+];
 
 /** Hebrew (עברית) */
-$specialPageAliases['he'] = array(
-	'GWToolset' => array( 'ארגז_כלי_גלאם_ויקי', 'ארגז_גלי_גלאם_ויקי' ),
-);
+$specialPageAliases['he'] = [
+	'GWToolset' => [ 'ארגז_כלי_גלאם_ויקי', 'ארגז_גלי_גלאם_ויקי' ],
+];
 
 /** Korean (한국어) */
-$specialPageAliases['ko'] = array(
-	'GWToolset' => array( 'GW도구모음' ),
-);
+$specialPageAliases['ko'] = [
+	'GWToolset' => [ 'GW도구모음' ],
+];
 
 /** Macedonian (македонски) */
-$specialPageAliases['mk'] = array(
-	'GWToolset' => array( 'GWАлатник' ),
-);
+$specialPageAliases['mk'] = [
+	'GWToolset' => [ 'GWАлатник' ],
+];
 
 /** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'GWToolset' => array( 'GWGereedschap' ),
-);
+$specialPageAliases['nl'] = [
+	'GWToolset' => [ 'GWGereedschap' ],
+];
 
 /** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = array(
-	'GWToolset' => array( 'GW工具集' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'GWToolset' => [ 'GW工具集' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎) */
-$specialPageAliases['zh-hant'] = array(
-	'GWToolset' => array( 'GW_工具組' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'GWToolset' => [ 'GW_工具組' ],
+];
