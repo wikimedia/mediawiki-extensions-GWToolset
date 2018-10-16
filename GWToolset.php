8998aefd50b12b280794f966b17d37c9202897fb
Kunal Mehta <legoktm@member.fsf.org>
2018-10-16 11:29:30 -0700

    Warn if legacy PHP entry point is used
    Bug: T140851
    Change-Id: Ied346fd12d2dc351c5e18cd68fbe43d96d3fc6ae

--- GWToolset.php
+++ GWToolset.php
@@ -5,10 +5,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['GWToolset'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['GWToolsetAlias'] = __DIR__ . '/GWToolset.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for GWToolset extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the GWToolset extension requires MediaWiki 1.25+' );
