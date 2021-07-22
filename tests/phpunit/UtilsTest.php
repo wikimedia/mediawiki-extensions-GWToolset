127e26c3a349bd22a95982d8c13522ed39e0af2a
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2021-07-22 09:18:02 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 36.0.0 → 37.0.0
    
    npm:
    * postcss: 7.0.35 → 7.0.36
      * https://npmjs.com/advisories/1693 (CVE-2021-23368)
    * glob-parent: 5.1.0 → 5.1.2
      * https://npmjs.com/advisories/1751 (CVE-2020-28469)
    * trim-newlines: 3.0.0 → 3.0.1
      * https://npmjs.com/advisories/1753 (CVE-2021-33623)
    
    Change-Id: I5f6a159355745e92165eb73a5abca648d392c90c

--- tests/phpunit/UtilsTest.php
+++ tests/phpunit/UtilsTest.php
@@ -15,7 +15,7 @@ class GWToolsetUtilsTest extends MediaWikiTestCase {
 	 */
 	protected $utils;
 
-	protected function setUp() : void {
+	protected function setUp(): void {
 		parent::setUp();
 		$this->utils = new Utils();
 	}
